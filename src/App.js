081a5e177aa2930d059b41bfe7ad41fa821e8453
rouxdev <aswigginton@gmail.com>
2021-03-02 23:13:43 -0700

    initial

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>roux.dev</h1>
+      <header className="App-header">
+        <p>
+          <code>Welcome and happy to have you!</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/andrew-wigginton/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
